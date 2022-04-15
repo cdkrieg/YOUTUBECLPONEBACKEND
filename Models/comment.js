ff27ecf60a8585df4070bb3874e61da20f43c4cb
William Hagan <williamhagan1728@yahoo.com>
2022-04-15 16:44:01 -0700

    update comment.js and index.js to include imports of reply.js

--- Models/comment.js
+++ Models/comment.js
@@ -2,20 +2,26 @@ const mongoose = require("mongoose");
 const Joi = require("joi");
 
 const commentSchema = new mongoose.Schema({
-    comment: {type: String, required: true, minlength:1, maxlength:250},
-    dateAdded: {type: Date, default: Date.now()},
-});
-
-function validateComment(comment){
+    name: { type: String, required: true, minlength: 2, maxlength: 255 },
+    body: { type: String, required: true, minlength: 2 },
+    like: { type: Boolean, required: true },
+    dislike: { type: Boolean, required: true },
+    dateAdded: { type: Date, default: Date.now() },
+    replies: [repliesSchema],
+  });
+  
+  function validateComment(comment) {
     const schema = Joi.object({
-        comment: Joi.string().min(2).max(250).required(),
-    })
-        return schema.validate(comment);
-}
-
-const Comment = mongoose.model('Comment', commentSchema);
-module.exports = {
+      name: Joi.string().min(2).max(255).required(),
+      body: Joi.string().required(),
+      like: Joi.boolean().required(),
+      dislike: Joi.boolean().required(),
+    });
+    return schema.validate(comment);
+  }
+  const Comment = mongoose.model("Comment", commentSchema);
+  module.exports = {
     Comment,
     validateComment,
     commentSchema,
-}; 
+  };
--- index.js
+++ index.js
@@ -1,50 +1,23 @@
 require("dotenv").config();
-const connectDb = require("./db/db");
+const connectDb = require("./startup/db");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const {google} = require("googleapis");
 
-const comments = require("./routes/comments")
-const apikey = "AIzaSyDAXtEJIM9IO2wD89wXhcRxmu72ryJGk2I";
-const baseApiUrl = "https://www.googleapis.com/youtube/v3";
-const youtube = google.youtube({
-  version: "v3",
-  auth: apikey,
-});
-
-app.get("/search-with-googleapis", async (req, res, next) => {
-  try {
-    const searchQuery = req.query.search_query;
-    const response = await youtube.search.list({
-      part: "snippet",
-      q: searchQuery,
-      type: "video",
-    });
-    const titles = response.data.items.map((item) => item.snippet.title);
-    res.send(titles);
-  } catch (error) {
-    next(err);
-  }
-});
-
-
-
-
-
-
-
+// const comments = require("./routes/comments");
+// const replies = require("./routes/replies.js");
 
 connectDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/comments", comments);
+// app.use('/api/comments', comments)
+// app.use('/api/replies',  replies);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running. Listening on PORT: ${PORT}`);
 });
-//nodemon install
+
