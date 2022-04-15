55edd977a9d1f6396a81fe3e57edba1970e16db7
William Hagan <williamhagan1728@yahoo.com>
2022-04-14 17:04:13 -0700

    install api from user story, continue comment code

--- index.js
+++ index.js
@@ -3,12 +3,44 @@ const connectDb = require("./db/db");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const {google} = require("googleapis");
+
+const comments = require("./routes/comments")
+const apikey = "AIzaSyDAXtEJIM9IO2wD89wXhcRxmu72ryJGk2I";
+const baseApiUrl = "https://www.googleapis.com/youtube/v3";
+const youtube = google.youtube({
+  version: "v3",
+  auth: apikey,
+});
+
+app.get("/search-with-googleapis", async (req, res, next) => {
+  try {
+    const searchQuery = req.query.search_query;
+    const response = await youtube.search.list({
+      part: "snippet",
+      q: searchQuery,
+      type: "video",
+    });
+    const titles = response.data.items.map((item) => item.snippet.title);
+    res.send(titles);
+  } catch (error) {
+    next(err);
+  }
+});
+
+
+
+
+
+
+
 
 connectDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/comments", comments);
 
 const PORT = process.env.PORT || 5000;
 
