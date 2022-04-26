2193e23220f8c48bb93a5f3693b3301d5b7492b6
cdkrieg <37251514+cdkrieg@users.noreply.github.com>
2022-04-25 19:35:44 -0500

    Update comments.js
    remove unused router methods

--- Routes/comments.js
+++ Routes/comments.js
@@ -5,7 +5,7 @@ const {append} = require("express/lib/response")
 const router = express.Router();
 
 //GET all comments
-//http://localhost:5000/api/comments
+//http://localhost:3007/api/comments
 router.get("/", async (req, res) => {
     try {
         let comments = await Comment.find();
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
 });
 
 //GET a comment by videoid
-//http://localhost:5000/api/comments/:videoId
+//http://localhost:3007/api/comments/:videoId
 router.get("/:videoId", async (req, res)=>{
     try {
         let comment = await Comment.find(req.params, {}, { _id: 0, videoId: 1});
@@ -56,17 +56,6 @@ router.put("/:id", async (req, res) => {
     }
 });
 
-//DELETE a Comment
-//http://localhost:5000/api/comments/6254e000fbe3ae214584dba7
-router.delete("/:id", async (req, res) => {
-    try {
-        let comment = await Comment.findByIdAndDelete(req.params.id);
-        if (!comment) return res.status(400).send(`Comment with id "${req.params.id}" does not exist!`);
-        return res.send(comment);
-    } catch (error) {
-        return res.status(500).send(`Internal Server Error: ${error}`);
-    }
-});
 
 //POST new Comment
 router.post("/", async (req, res) => {
@@ -80,70 +69,5 @@ router.post("/", async (req, res) => {
         return res.status(500).send(`Internal Server Error: ${error}`);
     }
 });
-
-//comment like
-//http://localhost:5000/api/comments/:commentId/commentLike
-router.put("/:commentId/commentLike", async (req, res)=>{
-    try {
-        let comment = await Comment.findById(req.params.commentId);
-        if (!comment)
-         return res
-         .status(400)
-         .send(`Comment with Id of ${req.params.commentId} does not exist!`);
-
-         comment.likes++;
-         await comment.save();
-
-         return res
-         .status(200)
-         .send(comment);        
-    } catch (error) {
-        return res
-        .status(500)
-        .send(`Internal Server Error: ${error}`);
-
-    }
-})
-// comment dislike
-//http://localhost:5000/api/comments/:commentId/commentDisLike
-router.put("/:commentId/commentDisLike", async (req, res)=>{
-    try {
-        let comment = await Comment.findById(req.params.commentId);
-        if (!comment)
-         return res
-         .status(400)
-         .send(`Comment with Id of ${req.params.commentId} does not exist!`);
-
-         comment.dislikes++;
-         await comment.save();
-
-         return res
-         .status(200)
-         .send(comment);        
-    } catch (error) {
-        return res
-        .status(500)
-        .send(`Internal Server Error: ${error}`);
-
-    }
-})
-
-//POST a reply to a comment
-//http://localhost:5000/api/comments/:commentId/replies/:replyId
-router.post("/:commentId/replies/:replyId", async(req,res)=>{
-    try {
-        let comment = await Comment.findById(req.params.commentId);
-        if (!comment) return res.status(400).send(`No comment with ID ${req.params.commentId}!`);
-
-        let reply = await Reply.findById(req.params.replyId);
-        if (!reply) return res.status(400).send(`Reply with ID ${req.params.replyId} does not exist!`);
-
-        comment.replies.push(reply);
-        await comment.save();
-        return res.send(comment.replies);
-    } catch (error) {
-        return res.status(500).send(`Internal Server Error: ${error}`);
-    }
-})
 
 module.exports = router;
